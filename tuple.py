056ad674b68e13953782fa7d3e0c9017f769b545
IshanToshniwal <ishantoshniwal31@gmail.com>
2024-06-04 21:30:21 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,8 @@
+tuple1 = (1, 2, 3, 4, 5, 1)
+# print(tuple1[1:4])
+# print(sorted(tuple1))
+# print(tuple1.index(1))
+# print(tuple1.count(1))
+# print(tuple1.index(1,1))
+# print(sorted(tuple1, reverse=True))
+# new = tuple(new) conversion to list to tuple
